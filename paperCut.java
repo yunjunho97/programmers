180ca0e2924937f9d4ad9c8f485ced27a3308323
yunjunhoo <ptheg@naver.com>
2022-11-18 23:24:20 +0900

    출처: 프로그래머스 코딩 테스트 연습 https://programmers.co.kr/learn/challenges

--- paperCut.java
+++ paperCut.java
@@ -1,5 +1,13 @@
 package programmers;
 
 public class paperCut {
-
+	public static void main(String[] args) {
+		
+	}
+	public int solution(int M, int N) {
+        int answer = 0;
+        answer += N-1;
+        answer += N*(M-1);
+        return answer;
+    }
 }
